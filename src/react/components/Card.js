fb42d5c491a5122097284d95a41b05c64d59dcfe
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-09 21:26:00 +0100

    Change email address

--- src/react/components/Card.js
+++ src/react/components/Card.js
@@ -5,7 +5,7 @@ const Card = () => {
     <section className="card">
       <h2>Website under construction.</h2>
       <p>Next scheduled update: 12 Nov. 2023</p>
-      <button onClick={() => window.location.href='mailto:eren.g94@gmail.com'}>Send me an email</button>
+      <button onClick={() => window.location.href='mailto:info@erengazioglu.com'}>Send me an email</button>
     </section>
   )
 }
