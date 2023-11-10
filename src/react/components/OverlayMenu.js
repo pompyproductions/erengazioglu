c77fb3b6da8f13965bf66349f948193457ed9a57
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-10 14:20:20 +0100

    Start working on reactive menu component

--- src/react/components/OverlayMenu.js
+++ src/react/components/OverlayMenu.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 const OverlayMenu = () => {
+  const [isActive, toggle] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("keypress", () => toggle(state => !state))
+  }, [])
+
   return (
-    <dialog className="overlay-menu" open>
+    <div className={`overlay-menu ${isActive ? "active" : ""}`}>
       <Card />
-    </dialog>
+    </div>
   )
 }
 
