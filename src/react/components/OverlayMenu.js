216c6169d226b7d8f3c95c3157ce203f6541bf7e
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-11 17:21:14 +0100

    show menu on key press

--- src/react/components/OverlayMenu.js
+++ src/react/components/OverlayMenu.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 const OverlayMenu = () => {
-  const [isActive, toggle] = useState(false);
+  const [isActive, toggle] = useState(true);
 
   useEffect(() => {
-    window.addEventListener("keypress", () => toggle(state => !state))
-  }, [])
+    const handleKeyPress = () => {
+      toggle((state) => !state);
+    };
+  
+    window.addEventListener("keypress", handleKeyPress);
+  
+    return () => {
+      window.removeEventListener("keypress", handleKeyPress);
+    };
+  }, []);
 
   return (
     <div className={`overlay-menu ${isActive ? "active" : ""}`}>
