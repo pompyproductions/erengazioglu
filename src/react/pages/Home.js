ad6d27602ebbeba74f0fb6863c89a84c5b7e7b81
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-12 22:44:25 +0100

    Add some page content

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -4,9 +4,20 @@ import Card from "../components/Card"
 const Home = () => {
   return (
     <main>
-      <Card title="Thanks for dropping by!">
-        <p>I am Eren Gazioglu, and this is the space where I keep </p>
+      <Card title="Thanks for stopping by!">
+        <p>I'm Eren Gazioglu, a web/software developer with a background in architecture.</p>
+        <p>If you're interested, you can find the source code for this website by following <a href="https://github.com/pompyproductions/erengazioglu" target="_blank">this link</a> <i>(opens a new tab/window).</i></p>
       </Card>
+      <div className="content">
+        <p>If you don't want to wait until the next update to see more, you can:</p>
+        <nav>
+          <ul>
+            <li><a href="https://github.com/pompyproductions">find my projects in my GitHub profile,</a></li>
+            <li>check out my <a href="https://codepen.io/pompyproductions" target="_blank">CodePen,</a></li>
+            <li><a href="mailto:hello@erengazioglu.com?subject=Let\'s get in touch">reach out</a> with an email</li>
+          </ul>
+        </nav>
+      </div>
     </main>
   )
 }
--- src/react/pages/Projects.js
+++ src/react/pages/Projects.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Card from "../components/Card";
+import { Link } from "react-router-dom"
 
 const Projects = () => {
   return (
     <main>
-      <Card />
+      <Card title="This page is under development.">
+        <p>Sorry for the inconvenience, check back soon!</p>
+        <Link to="/"><button>&lt; Go back to the homepage</button></Link>
+      </Card>
     </main>
   )
 }
