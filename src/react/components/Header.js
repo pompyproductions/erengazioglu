b1630da7f3fe2329bffcd8ebc88e4cf2f6ddc97d
Eren Gazioglu <pompyproduction@gmail.com>
2024-09-01 17:22:15 +0200

    Remove "under construction"

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -6,9 +6,9 @@ const Header = (props) => {
   return (
     <header>
       <Link to="/" className="logo">Eren Gazioglu</Link>
-      <Card title="Website under construction.">
-        <p>Last update: 28 Aug. 2024</p>
-        <p>Next scheduled update: 02 Sep. 2024</p>
+      <Card title="Status">
+        <p>Last update:<br/>01 Sep. 2024</p>
+        <p>Next scheduled update:<br/>01 Oct. 2024</p>
       </Card>
       <nav>
         <ul>
